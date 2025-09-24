b312ad1485a0d760539fdb36134578f4cc71a4fb
Mikel-67 <mgoyago25paag@ikzubirimanteo.com>
2025-09-24 09:46:19 +0200

    Pipe Ariketa

---
+++ Ariketa 2/Bezeroa/Bezeroa/Bezeroa/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipes;
+using System.IO;
+
+class Bezeroa
+{
+    static void Main()
+    {
+        using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "CalculadoraPipe", PipeDirection.InOut))
+        {
+            Console.WriteLine("Zerbitzariarekin konektatzen...");
+            pipeClient.Connect();
+
+            using (StreamReader reader = new StreamReader(pipeClient))
+            using (StreamWriter writer = new StreamWriter(pipeClient) { AutoFlush = true })
+            {
+                int operazioa = 0;
+
+                while (operazioa != -1)
+                {
+                    Console.WriteLine("\nOperacion a realizar: \n" +
+                        "1 - Suma\n" +
+                        "2 - Resta\n" +
+                        "3 - Multiplicar\n" +
+                        "4 - Dividir\n" +
+                        "5 - Potencia\n" +
+                        "(-1) Salir");
+
+                    Console.Write("Introduzca la operacion: ");
+                    operazioa = Convert.ToInt32(Console.ReadLine());
+
+                    if (operazioa == -1)
+                    {
+                        writer.WriteLine("-1");
+                        break;
+                    }
+
+                    Console.Write("Sartu lehenengo zenbakia: ");
+                    double znb1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Sartu bigarren zenbakia: ");
+                    double znb2 = Convert.ToDouble(Console.ReadLine());
+
+                    writer.WriteLine($"{operazioa} {znb1} {znb2}");
+
+                    string? emaitza = reader.ReadLine();
+                    if (emaitza == null)
+                    {
+                        Console.WriteLine("Errorea: ez da emaitzarik jaso zerbitzaritik.");
+                        break;
+                    }
+                    Console.WriteLine("Servitzariaren emaitza: " + emaitza);
+                }
+            }
+        }
+    }
+}
---
+++ Ariketa 2/Zerbitzaria/Zerbitzaria/Zerbitzaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO.Pipes;
+using System.IO;
+
+class Zerbitzaria
+{
+    static void Main()
+    {
+        using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("CalculadoraPipe", PipeDirection.InOut))
+        {
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Konexioa Konektatuta.");
+
+            using (StreamReader reader = new StreamReader(pipeServer))
+            using (StreamWriter writer = new StreamWriter(pipeServer) { AutoFlush = true })
+            {
+                string? bezeroMezua;
+                while ((bezeroMezua = reader.ReadLine()) != null)
+                {
+                    if (bezeroMezua == "-1") break;
+                    string[] partes = bezeroMezua.Split(' ');
+                    int operazioa = int.Parse(partes[0]);
+                    double znb1 = double.Parse(partes[1]);
+                    double znb2 = double.Parse(partes[2]);
+
+                    double emaitza = 0;
+
+                    switch (operazioa)
+                    {
+                        case 1: emaitza = znb1 + znb2; break;
+                        case 2: emaitza = znb1 - znb2; break;
+                        case 3: emaitza = znb1 * znb2; break;
+                        case 4:
+                            if (znb2 != 0) emaitza = znb1 / znb2;
+                            else { writer.WriteLine("Error: ezin da zati 0 egin"); continue; }
+                            break;
+                        case 5: emaitza = Math.Pow(znb1, znb2); break;
+                        default: writer.WriteLine("Ez da opzioa existitzen"); continue;
+                    }
+
+                    Console.WriteLine($"Emaitza: {emaitza}");
+                    writer.WriteLine(emaitza);
+                }
+            }
+        }
+
+        Console.WriteLine("Zerbitzaria itzalita.");
+    }
+}
