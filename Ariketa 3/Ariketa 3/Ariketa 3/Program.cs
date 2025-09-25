94d07f67366f16f0fcbbe212a9cc343c27ee752f
Mikel-67 <mgoyago25paag@ikzubirimanteo.com>
2025-09-25 10:31:54 +0200

    Ariketa 3 Bukatuta

---
+++ Ariketa 3/Ariketa 3/Ariketa 3/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO.Pipes;
+using System.IO;
+
+class Bezeroa
+{
+    static void Main()
+    {
+        using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "AzaleraKalkPipe", PipeDirection.InOut))
+        {
+            Console.WriteLine("Zerbitzariarekin konektatzen...");
+            pipeClient.Connect();
+
+            using (StreamReader reader = new StreamReader(pipeClient))
+            using (StreamWriter writer = new StreamWriter(pipeClient) { AutoFlush = true })
+            {
+                int figura = 0;
+
+                while (figura != -1)
+                {
+                    Console.WriteLine("\nZein figura geometrikoa daukazu: \n" +
+                        "1 - Zirkulua\n" +
+                        "2 - Triangelua\n" +
+                        "3 - Laukizuzena\n" +
+                        "4 - Pentagonoa\n" +
+                        "(-1) Salir");
+
+                    Console.Write("Sartu figuraren zenbakia: ");
+                    figura = Convert.ToInt32(Console.ReadLine());
+
+                    double znb1;
+                    double znb2;
+
+                    if (figura == -1)
+                    {
+                        writer.WriteLine("-1");
+                        break;
+                    }else if (figura == 4)
+                    {
+                        Console.Write("Sartu pentagonoaren apotema: ");
+                        znb1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Sartu pentagonoaren aldea: ");
+                        znb2 = Convert.ToDouble(Console.ReadLine());
+                    }else if (figura == 1)
+                    {
+                        Console.Write("Sartu zirkuluaren erradioa: ");
+                        znb1 = Convert.ToDouble(Console.ReadLine());
+                        znb2 = Math.PI;
+                    }
+                    else
+                    {
+                        Console.Write("Sartu figuraren altuera: ");
+                        znb1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Sartu figuraren oinarria: ");
+                        znb2 = Convert.ToDouble(Console.ReadLine());
+                    }
+
+                    writer.WriteLine($"{figura} {znb1} {znb2}");
+
+                    string? emaitza = reader.ReadLine();
+                    if (emaitza == null)
+                    {
+                        Console.WriteLine("Errorea: ez da emaitzarik jaso zerbitzaritik.");
+                        break;
+                    }
+                    Console.WriteLine("Servitzariaren emaitza: " + emaitza);
+                }
+            }
+        }
+    }
+}
---
+++ Ariketa 3/Ariketa 3/Serbitzaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO.Pipes;
+using System.IO;
+
+class Zerbitzaria
+{
+    static void Main()
+    {
+        using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("AzaleraKalkPipe", PipeDirection.InOut))
+        {
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Konexioa Konektatuta.");
+
+            using (StreamReader reader = new StreamReader(pipeServer))
+            using (StreamWriter writer = new StreamWriter(pipeServer) { AutoFlush = true })
+            {
+                string? bezeroMezua;
+                while ((bezeroMezua = reader.ReadLine()) != null)
+                {
+                    if (bezeroMezua == "-1") break;
+                    string[] datuak = bezeroMezua.Split(' ');
+                    int figura = int.Parse(datuak[0]);
+                    double znb1 = double.Parse(datuak[1]);
+                    double znb2 = double.Parse(datuak[2]);
+
+                    double emaitza = 0;
+
+                    switch (figura)
+                    {
+                        case 1: emaitza = (znb1*znb1) * znb2 ; break;
+                        case 2: emaitza = (znb1 * znb2) /2; break;
+                        case 3: emaitza = znb1 * znb2; break;
+                        case 4:
+                            emaitza = (5* znb2 * znb1) /2;
+                            break;
+                        default: writer.WriteLine("Ez da opzioa existitzen"); continue;
+                    }
+
+                    Console.WriteLine($"Emaitza: {emaitza}");
+                    writer.WriteLine(emaitza);
+                }
+            }
+        }
+        Console.WriteLine("Zerbitzaria itzalita.");
+    }
+}
