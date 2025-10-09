0848fbcf0f6096e4c4c0402c1c8266bc109f3e59
Mikel-67 <mgoyago25paag@ikzubirimanteo.com>
2025-10-09 09:53:15 +0200

    Hariak 3

---
+++ Hariak 2/Hariak 2/Hariak 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+        Thread batuketaHaria = new Thread(() => batuketa(a, b));
+    }
+
+    static void batuketa(int a, int b)
+    {
+        int emaitza = a + b;
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(emaitza);
+        }
+    }
+}
